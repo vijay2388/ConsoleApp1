4700adfa14d2748f48fc9eea24a0a200c0fd37bb
Vijay Vishwakarma <vijay2388@gmail.com>
2024-06-16 08:23:53 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Web.Script.Serialization;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get token
+            string accessToken = GetMSIToken("https://storage.azure.com/");
+
+            //create token credential
+            TokenCredential tokenCredential = new TokenCredential(accessToken);
+
+            //create storage credentials
+            StorageCredentials storageCredentials = new StorageCredentials(tokenCredential);
+
+            Uri blobAddress = 
+                new Uri("https://edurekaidentitystorage.blob.core.windows.net/test/AZ-204 Prerequisite.txt");
+
+            //create block blob using storage credentials
+            CloudBlockBlob blob = new CloudBlockBlob(blobAddress, storageCredentials);
+
+            //retrieve blob contents
+            Console.WriteLine(blob.DownloadText());
+            Console.ReadLine();
+        }
+
+        static string GetMSIToken(string resourceID)
+        {
+            string accessToken = string.Empty;
+
+            // Build request to acquire MSI token
+            HttpWebRequest request = 
+                (HttpWebRequest)WebRequest.Create("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=" + resourceID);
+            request.Headers["Metadata"] = "true";
+            request.Method = "GET";
+
+            try
+            {
+                // Call /token endpoint
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                // Pipe response Stream to a StreamReader, and extract access token
+                StreamReader streamResponse = new StreamReader(response.GetResponseStream());
+
+                string stringResponse = streamResponse.ReadToEnd();
+
+                JavaScriptSerializer j = new JavaScriptSerializer();
+
+                Dictionary<string, string> list = 
+                    (Dictionary<string, string>)j.Deserialize(stringResponse, typeof(Dictionary<string, string>));
+
+                accessToken = list["access_token"];
+                Console.WriteLine($"\n\nAccess Token :-\n\n{accessToken}\n\n");
+                return accessToken;
+            }
+            catch (Exception e)
+            {
+                string errorText = String.Format("{0} \n\n{1}", e.Message, e.InnerException != null ? e.InnerException.Message : "Acquire token failed");
+                return accessToken;
+            }
+        }
+    }
+}
